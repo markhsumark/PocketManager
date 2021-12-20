37110c77f5b308f125a886a3345403126422d72e
LJY <q892000@gmail.com>
2021-12-20 17:23:08 +0800

    init setting view

--- app/src/main/java/com/example/pocketmanager/MainActivity.java
+++ app/src/main/java/com/example/pocketmanager/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.pocketmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button google_button,hand_backup,auto_backup,edit_category,property;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auto_backup = findViewById(R.id.auto_backup);
+        auto_backup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity6.class);
+                startActivity(intent);
+            }
+        });
+        edit_category=findViewById(R.id.edit_catogory);
+        edit_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+        property=findViewById(R.id.property);
+        property.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity9.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pocketmanager/MainActivity9.java
@@ -0,0 +1,27 @@
+package com.example.pocketmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity9 extends AppCompatActivity {
+    Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main9);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity9.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
