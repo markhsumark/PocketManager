c15197cc915d648778515f24ed8dd1a5b975b10e
minjie Chiou <q1234565701@gmail.com>
2021-12-27 02:52:04 +0800

    add must actions of objects TODO: layout:assetLine button:monthPicker Interface optimization

--- app/src/main/java/com/example/pocketmanager/HomeActivity.java
+++ app/src/main/java/com/example/pocketmanager/HomeActivity.java
@@ -1,25 +1,35 @@
 package com.example.pocketmanager;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button adder;
+    private Button previousStep, nextStep, editor, adder;
+/*TODO assetLine
+    private Spinner assetName;
+    private TextView assetBalance;
+*/
     private RecyclerView externalRecyclerView, internalRecyclerView;
     private RecyclerView.LayoutManager exLayoutManager, inLayoutManager;
     private ExAdapter exAdapter;
@@ -31,6 +41,28 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
+///*  點選新增按鈕會跳轉頁面
+        previousStep = findViewById(R.id.previousStep);
+        previousStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+        nextStep = findViewById(R.id.nextStep);
+        nextStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+        editor = findViewById(R.id.editor);
+        editor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
         adder = findViewById(R.id.adder);
         adder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +72,11 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+/*TODO assetLine
+        assetName = findViewById(R.id.assetChooser);
+        assetBalance = findViewById(R.id.assetBalance);
+*/
+//*/
 /*
         internalRecyclerView = findViewById(R.id.internalRecyclerView);
         internalRecyclerView.setHasFixedSize(true);
@@ -66,20 +103,39 @@ public class HomeActivity extends AppCompatActivity {
         data = new LinkedList<>();
         for(int i=1; i<20; i++){
             HashMap<String, String> row = new HashMap<>();
-            row.put("category", "12/" + i / 2);
+            row.put("type", String.valueOf(i/10));
+            row.put("asset", String.valueOf(i/8));
+            row.put("category", String.valueOf(i/5));
             row.put("description", "i=" + i);
             row.put("money", "123" + i);
             data.add(row);
         }
     }
+/*
+    public void rackMonthPicker(View v){
 
+        RackMonthPicker rackMonthPicker = new RackMonthPicker(this)
+                .setLocale(Locale.ENGLISH)
+                .setPositiveButton(new DateMonthDialogListener() {
+                    @Override
+                    public void onDateMonth(int month, int startDate, int endDate, int year, String monthLabel) {
 
+                    }
+                })
+                .setNegativeButton(new OnCancelMonthDialogListener() {
+                    @Override
+                    public void onCancel(AlertDialog dialog) {
 
+                    }
+                }).show();
+    }
+*/
+///*  顯示RecyclerView
     private class ExAdapter extends RecyclerView.Adapter<ExAdapter.MyViewHolder>{
 
         class MyViewHolder extends RecyclerView.ViewHolder{
             public View itemView;
-            public  TextView category, description, money;
+            public TextView description, money, category;
 
             public MyViewHolder(View v){
                 super(v);
@@ -101,17 +157,22 @@ public class HomeActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull ExAdapter.MyViewHolder holder, int position) {
-            holder.category.setText(data.get(position).get("category"));
+            Resources res=getResources();
+            holder.category.setText(res.getStringArray(R.array.category)[Integer.parseInt(data.get(position).get("category"))]);
             holder.description.setText(data.get(position).get("description"));
             holder.money.setText(data.get(position).get("money"));
 
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    String type = data.get(position).get("type");
+                    String assets = data.get(position).get("asset");
                     String category = data.get(position).get("category");
                     String description = data.get(position).get("description");
                     String money = data.get(position).get("money");
                     Intent intent = new Intent(HomeActivity.this, AddOrEditActivity.class);
+                    intent.putExtra("type", type);
+                    intent.putExtra("asset", assets);
                     intent.putExtra("category", category);
                     intent.putExtra("description", description);
                     intent.putExtra("money", money);
@@ -126,6 +187,7 @@ public class HomeActivity extends AppCompatActivity {
             return data.size();
         }
     }
+//*/
 /*
     private class ExAdapter extends RecyclerView.Adapter<ExAdapter.MyViewHolder>{
 
