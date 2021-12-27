05bd35780eba1c973e624feca10d6f4d45637f60
minjie Chiou <q1234565701@gmail.com>
2021-12-27 16:57:24 +0800

    add TabView

---
+++ app/src/main/java/com/example/pocketmanager/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.pocketmanager;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.*;
+
+public class MainActivity extends TabActivity{
+
+    TabHost tabHost=null;      //选项卡控制器
+    TabHost.TabSpec tabSpecA,tabSpecB, tabSpecC=null;   //选项卡,这里选项卡最好不用混用，有几个选项卡就设置几个对象
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获得TabHost实例；
+        tabHost=getTabHost();
+        //获得TabHost.TabSpec对象实例；
+        tabSpecA=tabHost.newTabSpec("Home");
+        //为TabSpec对象设置指示器
+        tabSpecA.setIndicator("Home",getResources().getDrawable(android.R.drawable.ic_media_play));
+        //为选项卡设置内容，这里需要创建一个intent对象
+        Intent intentA=new Intent();
+        intentA.setClass(this, HomeActivity.class);
+        tabSpecA.setContent(intentA);
+/*
+        //for chart：
+        tabSpecB=tabHost.newTabSpec("Chart");
+        tabSpecB.setIndicator("Chart",getResources().getDrawable(android.R.drawable.ic_media_next));
+        Intent intentB=new Intent();
+        intentB.setClass(this, TabTwo.class);
+        tabSpecB.setContent(intentB);
+*/
+/*
+        //for setting：
+        tabSpecC=tabHost.newTabSpec("Setting");
+        tabSpecC.setIndicator("Setting",getResources().getDrawable(android.R.drawable.ic_media_next));
+        Intent intentC=new Intent();
+        intentC.setClass(this, TabThree.class);
+        tabSpecC.setContent(intentC);
+*/
+        //最后一步，把两个选项卡TabSpec添加到选项卡控件TabHost中
+        tabHost.addTab(tabSpecA);
+//        tabHost.addTab(tabSpecB);
+//        tabHost.addTab(tabSpecC);
+
+
+
+
+
+    }
+
+
+
+
+}
